71ca4e792f81c93939a5507d01d25835b2dec1f2
ashindin <shindin@rf.unn.ru>
2020-10-15 14:03:10 +0300

    return to signal_view

--- binary_file_options.py
+++ binary_file_options.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'binary_file_options.ui'
-#
-# Created by: PyQt5 UI code generator 5.9.2
-#
-# WARNING! All changes made in this file will be lost!
-
+import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_MainWindow(object):
@@ -21,21 +15,42 @@ class Ui_MainWindow(object):
         self.label_sr.setGeometry(QtCore.QRect(10, 90, 101, 17))
         self.label_sr.setObjectName("label_sr")
         self.label_nch = QtWidgets.QLabel(self.centralwidget)
-        self.label_nch.setGeometry(QtCore.QRect(10, 150, 171, 17))
+        self.label_nch.setGeometry(QtCore.QRect(10, 140, 171, 17))
         self.label_nch.setObjectName("label_nch")
         self.lineEdit_bd = QtWidgets.QLineEdit(self.centralwidget)
         self.lineEdit_bd.setGeometry(QtCore.QRect(220, 30, 51, 25))
         self.lineEdit_bd.setObjectName("lineEdit_bd")
+        self.lineEdit_bd.setText("16")
         self.lineEdit_sr = QtWidgets.QLineEdit(self.centralwidget)
         self.lineEdit_sr.setGeometry(QtCore.QRect(190, 90, 113, 25))
         self.lineEdit_sr.setObjectName("lineEdit_sr")
+        self.lineEdit_sr.setText("333333")
         self.lineEdit_nch = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit_nch.setGeometry(QtCore.QRect(220, 150, 51, 25))
+        self.lineEdit_nch.setGeometry(QtCore.QRect(220, 140, 51, 25))
         self.lineEdit_nch.setObjectName("lineEdit_nch")
+        self.lineEdit_nch.setText("1")
+
+        self.cancel_btn = QtWidgets.QPushButton(parent=self.centralwidget, text='Cancel')
+        self.cancel_btn.move(20, 175)
+        self.cancel_btn.clicked.connect(self.cancel_btn_callback)
+
+        self.apply_btn = QtWidgets.QPushButton(parent=self.centralwidget, text='Apply')
+        self.apply_btn.move(200, 175)
+        self.apply_btn.clicked.connect(self.apply_btn_callback)
+
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def cancel_btn_callback(self):
+        sys.exit(self.lineEdit_bd.getTe)
+
+    def apply_btn_callback(self):
+        bd = int(self.lineEdit_bd.text())
+        sr = int(self.lineEdit_sr.text())
+        nch = int(self.lineEdit_nch.text())
+        print(bd, sr, nch)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -45,12 +60,12 @@ class Ui_MainWindow(object):
         self.label_nch.setText(_translate("MainWindow", "number of I/Q channels:"))
 
 
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
+# if __name__ == "__main__":
+#     import sys
+#     app = QtWidgets.QApplication(sys.argv)
+#     MainWindow = QtWidgets.QMainWindow()
+#     ui = Ui_MainWindow()
+#     ui.setupUi(MainWindow)
+#     MainWindow.show()
+#     sys.exit(app.exec_())
 
--- signal_view.py
+++ signal_view.py
@@ -1,9 +1,12 @@
 import numpy as np
-from tkinter import filedialog
-from tkinter import *
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button #, RadioButtons
 import scipy.signal as ss
+
+from binary_file_options import Ui_MainWindow
 
 def get_raw_data_from_binary_file(fname,offset_samples,duration_samples,bit_depth,num_of_channels):
     f=open(fname,'rb')
@@ -29,16 +32,22 @@ def raw_to_complex_volts(data_raw,nc,v_range,bit_depth=16):
     return data_complex
 
 
-def main():
-    """This function does blah blah."""
-    root = Tk()
-    root.filename =  filedialog.askopenfilename(initialdir = ".",title = "Select file",filetypes = (("binary files",["*.bin","*.raw"]),("all files","*.*")))
-    print(root.filename)
-    
+def main():    
 
+    app = QtWidgets.QApplication(sys.argv)
+    fname = QtWidgets.QFileDialog.getOpenFileName(None, "Load binary file:", "", "Binary files (*.bin *.raw);; All files (*.*)")
+    print(fname)
+
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    
     # app = QtWidgets.QApplication(sys.argv)
     # window = SeeProcessing()
     # window.show()
     # app.exec_()
+
+    sys.exit(app.exec_())
 if __name__ == '__main__':
     main()
